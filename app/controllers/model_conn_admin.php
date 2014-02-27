843e13d3a6be9886bc976bdb17fa679c2e220365
Evan Caldwell <evancaldwell@yahoo.com>
2014-02-27 09:52:35 -0700

    moved the models into the controlers folder to test the requires

---
+++ app/controllers/model_conn_admin.php
@@ -0,0 +1,13 @@
+<?php
+$dsn = 'mysql:host=localhost;dbname=scribesketch';
+$username = 'php';
+$password = 'php-pass';
+
+try {
+    $db = new PDO($dsn, $username, $password); // creates a PDO object
+    echo 'connection successful';
+} catch (PDOException $exc) {
+    echo 'Connection failed';
+    exit;
+}
+?>
